49db752ed8bd80dee851f27057953385f7244625
Terry Moreland <tsmoreland@live.com>
2022-06-13 01:13:03 +0100

    add input dto for post/put operations

--- usersApi/app/datatransferobject.go
+++ usersApi/app/datatransferobject.go
@@ -24,3 +24,18 @@ func NewUserSummaryDto(id int64, name string) *UserSummaryDto {
 	u := UserSummaryDto{ID: id, Name: name}
 	return &u
 }
+
+type UserInputDto struct {
+	Name     string `json:"name"`
+	FullName string `json:"fullname"`
+}
+
+func (dto UserInputDto) BuildUser() *domain.User {
+	user := domain.User{ID: 0, Name: dto.Name, FullName: dto.FullName}
+	return &user
+}
+
+func (dto UserInputDto) UpdateUser(user domain.User) *domain.User {
+	updatedUser := domain.User{ID: user.ID, Name: dto.Name, FullName: dto.FullName}
+	return &updatedUser
+}
