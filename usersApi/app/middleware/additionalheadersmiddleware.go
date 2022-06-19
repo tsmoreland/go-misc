d63d291f8cd3f480cf835f22ca8a371f9d69fa5d
Terry Moreland <tsmoreland@live.com>
2022-06-19 12:45:16 +0100

    corrections to headers middleware

--- usersApi/app/middleware/additionalheadersmiddleware.go
+++ usersApi/app/middleware/additionalheadersmiddleware.go
@@ -5,17 +5,18 @@ import "net/http"
 // OwaspRecommendedApiHeaders adds recommended headers to response
 func OwaspRecommendedApiHeaders(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		h.ServeHTTP(w, r)
 
 		addHeaderIfNotPresent(w, "Cache-Control", "no-store")
 		addHeaderIfNotPresent(w, "X-Content-Type-Options", "nosniff")
 		addHeaderIfNotPresent(w, "X-Frame-Options", "DENY")
 		addHeaderIfNotPresent(w, "x-xss-protection", "1; mode=block")
-		addHeaderIfNotPresent(w, "Expect-CT", "max-age=0, enforce")
 		addHeaderIfNotPresent(w, "referrer-policy", "strict-origin-when-cross-origin")
 		addHeaderIfNotPresent(w, "X-Permitted-Cross-Domain-Policies", "none")
 		addHeaderIfNotPresent(w, "Content-Security-Policy",
 			"default-src: 'none'; FeaturePolicy: 'none'; Referrer-Policy: no-referrer")
+
+		// h.ServeHTTP writes body, after which we don't seem to be able to add headers which is a shame
+		h.ServeHTTP(w, r)
 	})
 }
 
