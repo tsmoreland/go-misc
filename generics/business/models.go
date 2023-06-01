70960e6494dd7ccb6d7ecd41bbe3647148d84a15
Terry Moreland <tsmoreland@live.com>
2023-06-02 00:57:15 +0100

    chapter 2 additions - adding cost method and Energy interface which is a contraint using a type-set to allow specific types rather than interfaces which could be considered more of a method set

--- generics/business/models.go
+++ generics/business/models.go
@@ -18,6 +18,18 @@ type Wind struct {
 	Netto float64
 }
 
+type Energy interface {
+	Solar | Wind // type-set which allows either Solar or Wind to be used in generics
+	Cost() float64
+}
+
+// Complex is another type-set interface but the use of ~ allows automatic extensibility,
+//
+//	should additional complex types be added to Go in the future
+type Complex interface {
+	~complex64 | ~complex128
+}
+
 // Print prints the information for a solar product.
 // The string is enriched with the required kineteco legal information.
 func (s *Solar) Print() string {
@@ -36,7 +48,11 @@ func PrintGeneric[T any](t T) string {
 	return fmt.Sprintf("%s - %v\n", kinetecoPrint, t)
 }
 
-func PrintSlice[T any](t []T) {
+// PrintSlice prints all Energy items in t to stdout, note the generic is
+//
+//	using T Energy rather than any for a more specific or
+//	constrained generic
+func PrintSlice[T Energy](t []T) {
 	for idx, itm := range t {
 		fmt.Printf("%d - %v", idx, PrintGeneric(itm))
 	}
